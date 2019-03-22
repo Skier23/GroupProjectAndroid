a43ad794feabff8e31b91ef5e9dd3752e9bac73e
clee98 <41557008+clee98@users.noreply.github.com>
2019-03-21 22:57:26 -0400

    Add files via upload

--- BlacksburgActivities/app/src/main/java/com/example/blacksburgactivities/ListFragment.kt
+++ BlacksburgActivities/app/src/main/java/com/example/blacksburgactivities/ListFragment.kt
@@ -1,11 +1,15 @@
 package com.example.blacksburgactivities
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.RecyclerView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,31 +20,39 @@ import androidx.recyclerview.widget.LinearLayoutManager
  */
 class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var name: String? = null
-    private var date: String? = null
-    private var location: String? = null
-    private var detail: String? = null
-
+    private val eventItems = ArrayList<EventItem>()
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        initArray(eventItems)
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         val adapter = ListAdapter()
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        adapter.setEvents(adapter.events1)
+        //adapter.setEvents(adapter.events1)
 
         return view
     }
 
+    private fun initArray(myDataset: MutableList<EventItem>) {
+        myDataset.clear()
+
+        myDataset.add(EventItem("Festival", "March 13", "Blacksburg", "Music Festival"))
+        myDataset.add(EventItem("Karaoke", "March 20", "Blacksburg", "Dance the night away"))
+        myDataset.add(EventItem("Fun times", "December 20", "Blacksburg", "Fun times fun vibes"))
+        myDataset.add(EventItem("Name", "January 15", "Blacksburg", "Music"))
+
+    }
+
 inner class ListAdapter: RecyclerView.Adapter<ListAdapter.EventViewHolder>() {
 
-    var events1 = mutableListOf<EventItem>()
+    var events1 = eventItems
 
     fun setEvents(events: MutableList<EventItem>) {
         for (i in 0 until 20) {
@@ -62,8 +74,14 @@ inner class ListAdapter: RecyclerView.Adapter<ListAdapter.EventViewHolder>() {
         holder.view.findViewById<TextView>(R.id.eventName).text = events1[position].name
 
         // TODO bundleOf here
+
         holder.itemView.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_listFragment_to_detailFragment)
+            view?.findNavController()?.navigate(R.id.action_listFragment_to_detailFragment, bundleOf(
+                "name" to eventItems[position].name,
+                "date" to eventItems[position].date,
+                "location" to eventItems[position].location,
+                "detail" to eventItems[position].detail
+            ))
         }
     }
 
